c603f27ccc7334f719328999d8aef50cc34b87d4
Dylan Palmboom <nofxboy1234@yahoo.ca>
2013-11-04 20:05:43 +0200

    Config and Resources/Scripts

---
+++ Config/tuning.lua
@@ -0,0 +1,25 @@
+-- This file is used for tuning variables.
+CameraLockMin = {
+  type = "float",
+  value = 0.0,
+}
+
+CameraLockMax = {
+  type = "float",
+  value = 30.0,
+}
+
+BlockyMaxSpeed = {
+  type = "float",
+  value = 20.0,
+}
+
+BlockyJumpTimeout = {
+  type = "float",
+  value = 0.1,
+}
+
+BlockyJumpHeight = {
+  type = "float",
+  value = 32.0
+}
